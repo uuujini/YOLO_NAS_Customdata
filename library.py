aa17686295e24d11ee2a899ca9304b08dabd6758
yujin <youjin010402@naver.com>
2024-03-31 15:42:39 +0900

    feat: 데이터셋 가공

---
+++ library.py
@@ -0,0 +1,23 @@
+from super_gradients.training import Trainer
+from super_gradients.training import dataloaders
+from super_gradients.training.dataloaders.dataloaders import (
+    coco_detection_yolo_format_train,
+    coco_detection_yolo_format_val
+)
+from super_gradients.training import models
+from super_gradients.training.losses import PPYoloELoss
+from super_gradients.training.metrics import (
+    DetectionMetrics_050,
+    DetectionMetrics_050_095
+)
+from super_gradients.training.models.detection_models.pp_yolo_e import PPYoloEPostPredictionCallback
+from tqdm.auto import tqdm
+
+import os
+import requests
+import zipfile
+import cv2
+import matplotlib.pyplot as plt
+import glob
+import numpy as np
+import random
